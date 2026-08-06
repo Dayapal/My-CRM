25c2b768a297cc362b9061903fe337224aef8f49
Daya Pal <dayapal12pc@gmail.com>
2026-08-06 17:28:46 +0530

    done changes in backend user controller.js

--- backend/src/modules/users/user.controller.ts
+++ backend/src/modules/users/user.controller.ts
@@ -1,10 +1,6 @@
-import {
-  Request,
-  Response,
-} from "express";
+import {Request,Response,} from "express";
 
-import { asyncHandler }
-from "../../utils/asyncHandler.js";
+import { asyncHandler }from "../../utils/asyncHandler.js";
 
 import * as UserService
 from "./user.service.js";
