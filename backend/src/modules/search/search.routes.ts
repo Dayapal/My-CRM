aa1bf668b1bfbc63c855db36093ea76a5368516f
Daya Pal <dayapal12pc@gmail.com>
2026-08-08 15:27:53 +0530

    : done changes in backend search route ts

--- backend/src/modules/search/search.routes.ts
+++ backend/src/modules/search/search.routes.ts
@@ -1,32 +1,15 @@
-import { Router }
-from "express";
+import { Router }from "express";
 
-import {
-  protect,
-} from "../../middleware/auth.middleware.js";
-
-import {
-  validate,
-} from "../../middleware/validate.middleware.js";
-
-import {
-  searchSchema,
-} from "./search.validation.js";
-
-import {
-  globalSearch,
-} from "./search.controller.js";
+import {protect,} from "../../middleware/auth.middleware.js";
+import {validate,} from "../../middleware/validate.middleware.js";
+import {searchSchema,} from "./search.validation.js";
+import {globalSearch,} from "./search.controller.js";
 
 const router =
   Router();
 
 router.use(protect);
 
-/*
-=========================================
-GET /search?q=john
-=========================================
-*/
 
 router.get(
   "/",
