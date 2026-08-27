26af82f323a5b93264afa50096b5e966f8a2820d
Daya Pal <dayapal12pc@gmail.com>
2026-08-27 15:25:58 +0530

    refactor: remove unnecessary whitespace in auth middleware

--- backend/src/modules/activities/activity.controller.ts
+++ backend/src/modules/activities/activity.controller.ts
@@ -1,48 +1,24 @@
-import { Request, Response }
-from "express";
+import { Request, Response }from "express";
 
-import { asyncHandler }
-from "../../utils/asyncHandler.js";
+import { asyncHandler } from "../../utils/asyncHandler.js";
 
-import * as ActivityService
-from "./activity.service.js";
+import * as ActivityService from "./activity.service.js";
 
 export const getActivities =
-  asyncHandler(
-    async (
-      req: Request,
-      res: Response
-    ) => {
-      const activities =
+  asyncHandler( async (req: Request,res: Response) => { const activities =
         await ActivityService.getRecentActivities(
-          req.user
-            .organizationId
+          req.user.organizationId
         );
-
       res.json({
         success: true,
-        data:
-          activities,
-      });
-    }
-  );
+        data:activities, }); });
 
-
-  
-
-  export const getUserActivities =
-  asyncHandler(
-    async (
-      req: Request,
-      res: Response
-    ) => {
-
+  export const getUserActivities =asyncHandler(async (req: Request,res: Response) => {
       const activities =
         await ActivityService.getUserActivities(
           req.user.organizationId,
           req.params.userId as string
         );
-
       res.json({
         success: true,
         data: activities,
